68f12b628433fa330fd1fc99c35ceaeaeebc6dc5
FeelsGo-od <andrew.sokolowsky@gmail.com>
2022-02-28 02:21:39 +0100

    completed header

---
+++ mytemplate-main/index.php
@@ -0,0 +1,127 @@
+<?php // этот php-код компилирует less/style.less в css/style.css	
+	require "./assets/less/lessc.inc.php";	
+	function autoCompileLess($inputFile, $outputFile) {
+	  // load the cache
+	  $cacheFile = $inputFile.".cache";
+	  if (file_exists($cacheFile)) {
+		$cache = unserialize(file_get_contents($cacheFile));
+	  } else {
+		$cache = $inputFile;
+	  }
+	  $less = new lessc;
+	  $newCache = $less->cachedCompile($cache);
+	  if (!is_array($cache) || $newCache["updated"] > $cache["updated"]) {
+		file_put_contents($cacheFile, serialize($newCache));
+		file_put_contents($outputFile, $newCache['compiled']);
+	  }
+	}
+	autoCompileLess('./assets/less/style.less', './assets/css/style.css');
+// class="col-xs-6 wow fadeIn" data-wow-delay="0.3s" data-wow-duration="0.6s"
+?>
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--<meta name="viewport" content="width=1200px">-->
+
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+<!--<link rel="apple-touch-icon" href="ссылка на фавикон для apple">
+	<link rel="apple-touch-icon" sizes="72x72" href="ссылка на фавикон для apple">
+	<link rel="apple-touch-icon" sizes="114x114" href="ссылка на фавикон для apple"> -->
+    
+	<!-- <meta property="og:image" content="ссылка на картинку для превью"> -->
+    <link rel="stylesheet" href="./assets/css/style.css" type="text/css" />
+</head>
+
+<body>
+    <div class="wrapper">
+        <div id="mobile-menu">
+            <div class="block df">
+                <ul>
+                    <li><a href=""><span>Link</span></a></li>
+                    <li><a href=""><span>Link</span></a></li>
+                    <li><a href=""><span>Link</span></a></li>
+                    <li><a href=""><span>Link</span></a></li>
+                </ul>
+            </div>
+        </div>
+        <header>
+            <div class="container">
+                <div class="line1">
+                    <div class="container">
+                        <div class="block">
+                            <nav>
+                                <ul>
+                                    <li>О проекте</li>
+                                    <li>Апартаменты</li>
+                                    <li>Пентхаусы</li>
+                                </ul>
+                            </nav>
+                            <div class="phone">+7 612 210 42 42</div>
+                            <div class="button">Записаться</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="line2">
+                    <div class="container">
+                        <div class="block df">
+                            <a class="logo" href="">
+                                ZEROCLUS
+                            </a>
+                            <span>
+                                Идеальное расположение в тихом 
+                                переулке обеспечивает ощущение клубности и приватности владельцам 
+                                дома Zeroclus.
+                            </span>
+                            <div class="menu">
+                                <!-- Кнопка Мобильного Меню -->
+                                <button id="burger-menu">
+                                    <div class="box box_item1"></div>
+                                    <div class="box box_item2"></div>
+                                    <div class="box box_item3"></div>
+                                </button>
+                            </div>                    
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <section class="sec1">
+            <div class="container">
+               <img src="assets/img/bg.jpg" alt="">
+            </div>
+        </section>
+        <section class="sec2">
+            <div class="container">
+
+            </div>
+        </section>
+        <section class="sec3">
+            <div class="container">
+
+            </div>
+        </section>
+        <section class="sec4">
+            <div class="container">
+
+            </div>
+        </section>
+        <section class="sec5">
+            <div class="container">
+
+            </div>
+        </section>
+       
+        <footer>
+            <div class="container">
+                <p>footer</p>
+            </div>
+        </footer>
+    </div>
+
+    <?php require "modal.php"?>
+    <?php require "footer.php"?>
